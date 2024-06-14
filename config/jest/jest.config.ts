64b24c53c90f48b96e3ab9697d51aa6b8d9cd227
Ваше Имя <your@msddsdssdail.here>
2024-06-14 22:53:45 +0300

    dfsfd

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -37,7 +37,6 @@ export default {
     },
     // Indicates whether the coverage information should be collected while executing the test
     // collectCoverage: false,
-
     // An array of glob patterns indicating a set of files for which coverage information should be collected
     // collectCoverageFrom: undefined,
 
