1d5af2d4a20344a30d2827cb24db83b9432f1fd8
Ваше Имя <your@msddsdssdail.here>
2024-06-14 22:54:39 +0300

    sdds

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -39,7 +39,6 @@ export default {
     // collectCoverage: false,
     // An array of glob patterns indicating a set of files for which coverage information should be collected
     // collectCoverageFrom: undefined,
-
     // The directory where Jest should output its coverage files
     // coverageDirectory: undefined,
 
