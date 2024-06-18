e247f3ec3f947e5e288e1e4870d4e79c56c53ff7
1D1D1D1D1D1D <idclol@bk.ru>
2024-06-18 22:23:00 +0300

    TEST

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,19 +1,18 @@
-import { fireEvent, screen} from "@testing-library/react"
+import { fireEvent, screen } from '@testing-library/react';
+import Sidebar from './Sidebar';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 import '@testing-library/jest-dom'
-import Sidebar from "./Sidebar";
-import { renderWitchTraslation } from "shared/lib/classNames/tests/renderWithTranslation";
-describe('Sidebar', () => { 
+describe('Sidebar', () => {
     test('with only first param', () => {
-        renderWitchTraslation(<Sidebar/>)
-        expect(screen.getByTestId('sidebar')).toBeInTheDocument()
-    });
-    test('test toggle ', () => {
-        renderWitchTraslation(<Sidebar/>)
-        const tglBtn = screen.getByTestId('sidebar-toggle')
-        expect(screen.getByTestId('sidebar')).toBeInTheDocument()
-        fireEvent.click(tglBtn)
-        expect(screen.getByTestId('sidebar')).toHaveClass('collapsed')
+        componentRender(<Sidebar />, );
+        expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
 
-  
+    test('test toggle', () => {
+        componentRender(<Sidebar />);
+        const toggleBtn = screen.getByTestId('sidebar-toggle');
+        expect(screen.getByTestId('sidebar')).toBeInTheDocument();
+        fireEvent.click(toggleBtn);
+        expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
+    });
 });
