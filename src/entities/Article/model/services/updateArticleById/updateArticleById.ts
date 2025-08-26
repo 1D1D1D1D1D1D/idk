c8de75765c41425ce9e84ce0c02b8500d30cbb4d
1D1D1D1D1D1D <idclol@bk.ru>
2025-08-27 00:39:20 +0300

    Update updateArticleById.ts

--- src/entities/Article/model/services/updateArticleById/updateArticleById.ts
+++ src/entities/Article/model/services/updateArticleById/updateArticleById.ts
@@ -1,17 +1,19 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { ThunkConfig } from 'app/providers/StoreProvider';
 import { Article } from '../../types/article';
-import { getArticleDetailsForm } from '../../selectors/articleDetails';
+import { getArticleDetailsData, getArticleDetailsForm } from '../../selectors/articleDetails';
 
 export const updateArticleById = createAsyncThunk<Article, void, ThunkConfig<string>>(
     'articleDetails/updateArticleById',
     async (articleId, thunkAPI) => {
         const { extra, rejectWithValue, getState } = thunkAPI;
         const formData = getArticleDetailsForm(getState())
+        const data = getArticleDetailsData(getState())
+
         console.log(formData);
 
         try {
-            const response = await thunkAPI.extra.api.patch<Article>(`/articles/${formData?.id}`, formData);
+            const response = await thunkAPI.extra.api.patch<Article>(`/articles/${data?.id}`, formData);
             if (!response.data) {
                 throw new Error();
             }
